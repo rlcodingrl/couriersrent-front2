2fb38853250d1b7be6ff93a3ce4bda28163b5df5
Ralph Lauren <rl@example.com>
2022-12-28 22:17:45 +0200

    refactor: little design

--- src/components/Main/Couriers/Couriers.js
+++ src/components/Main/Couriers/Couriers.js
@@ -1,67 +1,39 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 
 import "./Couriers.css";
 
 import CouriersNav from "./CouriersNav";
 import CouriersList from "./CouriersList/CouriersList";
-
-import { UserContext } from "../../app/app";
-
-// import NewCourier from "./NewCourier";
-
 import NFECourier from "./NFECourier"
 
 export const StatusContext = React.createContext({});
-// export const fullInfoContext = React.createContext({});
 export const newCourierContext = React.createContext({});
-
 export const courierUpdateContext = React.createContext({});
-
 export const nFECourierContext = React.createContext({});
 
 const Couriers = ({status}) => {
 
   const [courierStatus]=useState(status)
-  const [fullInfo,setFullInfo] = useState({active: false, userId: ''})  // this state is object about component fullInfo that describe {active,id}
-  // const [newCourier,setNewCourier] = useState(false) // this state is object about component newCourier that describe if it active
-  
   const [nFECourier, setNFECourier]=useState({active: false, courierId: ''})
-
-  // eslint-disable-next-line
-  const {user} = useContext(UserContext);
-
   const [courierCounter, setCourierCounter] = useState(0);
-  
 
   return (
-    <StatusContext.Provider value={courierStatus}>
-    {/* <fullInfoContext.Provider value={{fullInfo, setFullInfo}}> */}
-    {/* <newCourierContext.Provider value={{newCourier,setNewCourier}}> */}
-    <courierUpdateContext.Provider value={ {courierCounter, setCourierCounter} }>
 
+    <StatusContext.Provider value={courierStatus}>
+    <courierUpdateContext.Provider value={ {courierCounter, setCourierCounter} }>
     <nFECourierContext.Provider value={{nFECourier, setNFECourier}}>
     
-
       <div className="couriers">
-
-        {/* this is couriers comp {status}  */}
        
         <CouriersNav />
         <CouriersList />
 
       </div>
-      {/* {console.log(fullInfo)} */}
-      {/* {fullInfo.active===true&&<FullInfoCourier/>} */}
+
       {(nFECourier.active!==false)&&<NFECourier/>}
-      {/* <NewCourier/> */}
-
-
 
     </nFECourierContext.Provider>
-
     </courierUpdateContext.Provider>
-    {/* </newCourierContext.Provider> */}
-    {/* </fullInfoContext.Provider>  */}
     </StatusContext.Provider>
   );
 };
--- src/components/Main/Couriers/CouriersNav/CouriersNav.js
+++ src/components/Main/Couriers/CouriersNav/CouriersNav.js
@@ -2,23 +2,18 @@
 import React, { useContext } from "react";
 
 import "./CouriersNav.css";
+import AddIcon from "../../../../assets/AddIcon";
 
 import { UserContext } from "../../../app/app";
-// import { newCourierContext } from "../Couriers";
-import AddIcon from "../../../../assets/AddIcon";
 import { nFECourierContext } from "../Couriers";
 
 const CouriersNav = () => {
 
   const {user} = useContext(UserContext)
-  // console.log(user.role)
-  // const {setNewCourier} = useContext(newCourierContext)
-
   const {setNFECourier} = useContext(nFECourierContext)
 
   return (
     <div className="couriers-nav">
-      {/* {user.role==="admin"?<span className="def-btn" onClick={()=>{setNewCourier(true)}}>{AddIcon()}</span>:null} */}
       {user.role==="admin"?<span className="def-btn" onClick={()=>{setNFECourier({active: 'new', courierId: ''})}}>{AddIcon()}</span>:null}
     </div>
   );
