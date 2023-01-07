b00833765eee25b73666388fe0a995f08b5ed297
Ralph Lauren <rl@example.com>
2023-01-07 20:05:08 +0200

    refactor: before deleting userContext

--- src/components/Header/Profile/Profile.js
+++ src/components/Header/Profile/Profile.js
@@ -4,17 +4,18 @@ import { logout } from "../../../services/auth/logout";
 import { NavLink } from "react-router-dom";
 
 // redux
-import {useDispatch} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setAuthFalse } from "../../../store/ifAuthReducer";
 import { clearUserDataAction } from "../../../store/userReducer";
 
 import "./Profile.css";
 
 const Profile = () => {
-  const { user, setUser, setIfAuthenticated } =
+  const { setUser } =
     useContext(UserContext);
   // redux
   const dispatch = useDispatch();
+  const user = useSelector(state=>state.user)
 
   // console.log(user);
   return (
