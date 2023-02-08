225afe754c04cd10c5fdccb3ce28b401db3e8047
Ralph Lauren <rl@example.com>
2023-02-08 14:33:17 +0200

    feat: render error fixes

--- src/assets/YesIcon/YesIcon.jsx
+++ src/assets/YesIcon/YesIcon.jsx
@@ -12,7 +12,7 @@ function AddIcon() {
       width="100%"
       height="100%"
       viewBox="0 0 96 96"
-      enable-background="new 0 0 96 96"
+      enableBackground="new 0 0 96 96"
     >
       <g>
         <path
